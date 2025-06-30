861a6f2d6bc1f4906694e2b54ae80d59e12a0371
kash <iam8139@gmail.com>
2025-06-30 22:18:03 +0530

    support for 2D matrix 7*7

--- Grid.java
+++ Grid.java
@@ -1,9 +1,12 @@
 import java.util.*;
 public class Grid {
     private ArrayList<String> locations;
-    public Grid(ArrayList<String> locations) {
-        this.locations = locations;
+    private String name;
+    public Grid(String name) {
+        this.name = name;
     }
+
+    public void setLocations(ArrayList<String> locations) {this.locations=locations;}
     public String checkYourself(String position) {
         int loc = locations.indexOf(position);
         String res = "Miss";
@@ -12,6 +15,7 @@ public class Grid {
 
             if (locations.isEmpty()) {
                 res = "Kill";
+                System.out.println("Ouch! you sunk " + this.name + " : (");
             } else {
                 res = "Hit";
             }
--- HitOrMissGameStart.java
+++ HitOrMissGameStart.java
@@ -1,26 +1,65 @@
 import java.util.*;
 public class HitOrMissGameStart {
-
     private static final GameHelper gameHelper = new GameHelper();
+    private ArrayList<Grid> grids = new ArrayList<>(3);
+    private int numbOfGuesses = 0;
+    
+    private void setUpGame() {
+        Grid grid1 = new Grid("Pets.com");
+        Grid grid2 = new Grid("eToys.com");
+        Grid grid3 = new Grid("Go.com");
+        grids.add(new Grid("Pets.com"));
+        grids.add(new Grid("eToys.com"));
+        grids.add(new Grid("Go.com"));
 
-    public static void main(String[] args) {
-        int random = (int)(Math.random() * 5);
-        ArrayList<String> locations = new ArrayList<>();
-        locations.add(String.valueOf(random));
-        locations.add(String.valueOf(random+1));
-        locations.add(String.valueOf(random+2));
-        int numOfGuesses = 0;
-        Grid grid = new Grid(locations);
-        boolean isAlive = true;
+        System.out.println("Your goal is to sink three dot coms.");
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Try to sink them all in the fewest number of guesses");
 
-        while (isAlive) {
-            String index = gameHelper.getUserInput("Please Enter Number: ");
-            String res = grid.checkYourself(index);
-            numOfGuesses++;
-            if (res.equals("Kill")) {
-                isAlive = false;
-                System.out.println("You took " + numOfGuesses + " guesses");
+        for (Grid grid : grids) {
+            ArrayList<String> newLocation = gameHelper.placeDotCom(3);
+            grid.setLocations(newLocation);
+        }
+    }
+
+    private void startPlaying() {
+        while(!grids.isEmpty()) {
+            String userGuess = gameHelper.getUserInput("Enter a Guess");
+            checkUserGuess(userGuess);
+        }
+
+        finishGame();
+    }
+
+    private void checkUserGuess(String userGuess) {
+        numbOfGuesses++;
+        String result = "miss";
+        for (int i = 0; i < grids.size(); i++) {
+            result = grids.get(i).checkYourself(userGuess);
+            if (result == "Hit") break;
+            else if (result == "Kill") {
+                grids.remove(i);
+                break;
             }
         }
+
+        System.out.println(result);
+    }
+
+    private void finishGame() {
+        System.out.println("All Dot Coms are dead! Your stock is now worthless.");
+        if (numbOfGuesses <= 18) {
+            System.out.println("It only took you " + numbOfGuesses + " guesses.");
+            System.out.println(" You got out before your options sank.");
+        } else {
+            System.out.println("Took you long enough. "+ numbOfGuesses + " guesses.");
+            System.out.println("Fish are dancing with your options");
+        }
+    }
+    public static void main(String[] args) {
+        HitOrMissGameStart game = new HitOrMissGameStart();
+        game.setUpGame();
+        game.startPlaying();
+        game.finishGame();
     }
 }
